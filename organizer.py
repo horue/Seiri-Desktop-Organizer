e1deaa202a9b6680f831321d42f0f1550868a694
Jorge <jorgejapao00@gmail.com>
2024-04-18 14:46:21 -0300

    Organizer created with output paths

---
+++ organizer.py
@@ -0,0 +1,13 @@
+import os
+
+
+
+img_types = ['.png', '.jpg', '.webp', '.JPG', '.JPEG']
+docs_tyopes = []
+
+
+
+initial=os.path.join(os.path.expanduser("~"), "Desktop")
+output_img=os.path.join(os.path.expanduser("~"), "Images\\Organized Images")
+output_docs=os.path.join(os.path.expanduser("~"), "Documents\\Organized Documents")
+output_videos=os.path.join(os.path.expanduser("~"), "Videos\\Organized Videos")
