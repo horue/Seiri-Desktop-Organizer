3dfeba57209acfbeef43e0772824700232e5edc3
Jorge <jorgejapao00@gmail.com>
2024-04-18 14:55:00 -0300

    Audio path added

--- organizer.py
+++ organizer.py
@@ -3,7 +3,7 @@ import os
 
 
 img_types = ['.png', '.jpg', '.webp', '.JPG', '.JPEG']
-docs_tyopes = []
+docs_tyopes = ['.DOC', '.DOCX', '.ODT', '.pdf', '.PPT', '.PPTX', '.txt']
 
 
 
@@ -11,3 +11,4 @@ initial=os.path.join(os.path.expanduser("~"), "Desktop")
 output_img=os.path.join(os.path.expanduser("~"), "Images\\Organized Images")
 output_docs=os.path.join(os.path.expanduser("~"), "Documents\\Organized Documents")
 output_videos=os.path.join(os.path.expanduser("~"), "Videos\\Organized Videos")
+output_audios=os.path.join(os.path.expanduser('~'), "\\Music")
