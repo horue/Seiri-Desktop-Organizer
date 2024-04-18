9b54a34cca533958c90bade01d1acee94f161fa0
Jorge <jorgejapao00@gmail.com>
2024-04-18 19:13:14 -0300

    PSD files added

--- organizer.py
+++ organizer.py
@@ -4,6 +4,7 @@ import os
 
 img_types = ['.png', '.jpg', '.webp', '.JPG', '.JPEG']
 docs_tyopes = ['.DOC', '.DOCX', '.ODT', '.pdf', '.PPT', '.PPTX', '.txt']
+ps_files = ['.psd', '.PSD']
 
 
 
@@ -11,4 +12,9 @@ initial=os.path.join(os.path.expanduser("~"), "Desktop")
 output_img=os.path.join(os.path.expanduser("~"), "Images\\Organized Images")
 output_docs=os.path.join(os.path.expanduser("~"), "Documents\\Organized Documents")
 output_videos=os.path.join(os.path.expanduser("~"), "Videos\\Organized Videos")
-output_audios=os.path.join(os.path.expanduser('~'), "\\Music")
+output_audios=os.path.join(os.path.expanduser('~'), "\\Music\\Organized Audios")
+output_photoshop=os.path.join(os.path.expanduser('~'), '\\Documents\\Organized PSDs')
+
+
+
+print(os.listdir(initial))
