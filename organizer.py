41b5b3717d3b260d6162c79c332b7cfb24ab9cd0
Jorge <jorgejapao00@gmail.com>
2024-04-18 20:02:00 -0300

    Organization for txt working

--- organizer.py
+++ organizer.py
@@ -1,20 +1,43 @@
 import os
+import shutil
 
 
 
-img_types = ['.png', '.jpg', '.webp', '.JPG', '.JPEG']
-docs_tyopes = ['.DOC', '.DOCX', '.ODT', '.pdf', '.PPT', '.PPTX', '.txt']
+img_types =('.png', '.jpg', '.webp', '.JPG', '.JPEG', '.txt')
+docs_types = ['.DOC', '.DOCX', '.ODT', '.pdf', '.PPT', '.PPTX', '.txt', 'docx']
 ps_files = ['.psd', '.PSD']
 
 
 
-initial=os.path.join(os.path.expanduser("~"), "Desktop")
-output_img=os.path.join(os.path.expanduser("~"), "Images\\Organized Images")
-output_docs=os.path.join(os.path.expanduser("~"), "Documents\\Organized Documents")
-output_videos=os.path.join(os.path.expanduser("~"), "Videos\\Organized Videos")
-output_audios=os.path.join(os.path.expanduser('~'), "\\Music\\Organized Audios")
-output_photoshop=os.path.join(os.path.expanduser('~'), '\\Documents\\Organized PSDs')
+initial=os.path.join(os.path.expanduser("~"), "Music")
+output_img=os.path.join(os.path.expanduser("~"), "Organized\\Organized Images")
+output_docs=os.path.join(os.path.expanduser("~"), "Organized\\Organized Documents")
+output_videos=os.path.join(os.path.expanduser("~"), "Organized\\Organized Videos")
+output_audios=os.path.join(os.path.expanduser('~'), "Organized\\Organized Audios")
+output_photoshop=os.path.join(os.path.expanduser('~'), 'Organized\\Organized PSDs')
+
+
+if not os.path.exists(output_img):
+    os.makedirs(output_img)
 
 
 
-print(os.listdir(initial))
+
+files = os.listdir(initial)
+img_files = [f for f in files if os.path.splitext(f)[1].lower() in img_types]
+doc_files = [f for f in files if os.path.splitext(f)[1].lower in docs_types]
+psds = [f for f in files if os.path.splitext(f)[1].lower() in ps_files]
+
+
+
+for img_file in img_files:
+    shutil.move(os.path.join(initial, img_file), output_img)
+
+
+for doc_file in doc_files:
+    shutil.move(os.path.join(initial, doc_file), output_docs)
+
+
+
+
+print(psds)
