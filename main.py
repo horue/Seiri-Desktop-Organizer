7dd5d263d1fe1c07e7bd78f61f16e076d3c5f405
Jorge <jorgejapao00@gmail.com>
2024-05-03 19:04:14 -0300

    Time for refreshed changed to 2

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from organizer import main
 import time
 
-
 while True:
     try:
         main()
-        time.sleep(5)
+        time.sleep(2)
     except Exception as e:
         print(f'Unexpected error: {e}')
