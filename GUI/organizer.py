82d40494bff4c221fe7b98c65911c38ba0e0c15e
Jorge <jorgejapao00@gmail.com>
2024-05-03 18:33:00 -0300

    Initial variable removed, changed to source path from GUI

--- GUI/organizer.py
+++ GUI/organizer.py
@@ -1,7 +1,5 @@
 import os
 import shutil
-from gui import source_path
-
 
 
 img_types =('.png', '.jpg', '.webp', '.JPG', '.JPEG')
@@ -13,7 +11,6 @@ zip_files = ('.rar', '.7z', '.zip')
 code_files = ('.py', '.jar', '.c')
 
 
-initial=os.path.join(os.path.expanduser("~"), source_path)
 output_img=os.path.join(os.path.expanduser("~"), "Documents\\Organized\\Organized Images")
 output_docs=os.path.join(os.path.expanduser("~"), "Documents\\Organized\\Organized Documents")
 output_videos=os.path.join(os.path.expanduser("~"), "Documents\\Organized\\Organized Videos")
@@ -29,7 +26,7 @@ def create_path():
             os.makedirs(path)
 
 
-files = os.listdir(initial)
+files = os.listdir(source_path)
 img_files = [f for f in files if os.path.splitext(f)[1].lower() in img_types]
 doc_files = [f for f in files if os.path.splitext(f)[1].lower() in docs_types]
 video_files = [f for f in files if os.path.splitext(f)[1].lower() in video_types]
@@ -38,56 +35,56 @@ psds = [f for f in files if os.path.splitext(f)[1].lower() in ps_files]
 zips = [f for f in files if os.path.splitext(f)[1].lower() in zip_files]
 codes = [f for f in files if os.path.splitext(f)[1].lower() in code_files]
 
-def organize_files():
+def organize_files(source_path):
     for img_file in img_files:
         file = os.path.join(output_img, os.path.basename(img_file))
         if not os.path.exists(file):
-            shutil.move(os.path.join(initial, img_file), output_img)
+            shutil.move(os.path.join(source_path, img_file), output_img)
         elif os.path.exists(file):
-            shutil.move(os.path.join(initial, img_file), os.path.join(output_img, 'copy_of_' + img_file))
+            shutil.move(os.path.join(source_path, img_file), os.path.join(output_img, 'copy_of_' + img_file))
 
     for doc_file in doc_files:
         file = os.path.join(output_docs, os.path.basename(doc_file))
         if not os.path.exists(file):
-            shutil.move(os.path.join(initial, doc_file), output_docs)
+            shutil.move(os.path.join(source_path, doc_file), output_docs)
         elif os.path.exists(file):
-            shutil.move(os.path.join(initial, doc_file), os.path.join(output_docs, 'copy_of_' + doc_file))
+            shutil.move(os.path.join(source_path, doc_file), os.path.join(output_docs, 'copy_of_' + doc_file))
 
     for video_file in video_files:
         file = os.path.join(output_videos, os.path.basename(video_file))
         if not os.path.exists(file):
-            shutil.move(os.path.join(initial, video_file), output_videos)
+            shutil.move(os.path.join(source_path, video_file), output_videos)
         elif os.path.exists(file):
-            shutil.move(os.path.join(initial, video_file), os.path.join(output_videos, 'copy_of_' + video_file))
+            shutil.move(os.path.join(source_path, video_file), os.path.join(output_videos, 'copy_of_' + video_file))
     
 
     for audio_file in audio_files:
         file = os.path.join(output_audios, os.path.basename(audio_file))
         if not os.path.exists(file):
-            shutil.move(os.path.join(initial, audio_file), output_audios)
+            shutil.move(os.path.join(source_path, audio_file), output_audios)
         elif os.path.exists(file):
-            shutil.move(os.path.join(initial, audio_file), os.path.join(output_audios, 'copy_of_' + audio_file))
+            shutil.move(os.path.join(source_path, audio_file), os.path.join(output_audios, 'copy_of_' + audio_file))
 
     for psd_file in psds:
         file = os.path.join(output_photoshop, os.path.basename(psd_file))
         if not os.path.exists(file):
-            shutil.move(os.path.join(initial, psd_file), output_photoshop)
+            shutil.move(os.path.join(source_path, psd_file), output_photoshop)
         elif os.path.exists(file):
-            shutil.move(os.path.join(initial, psd_file), os.path.join(output_photoshop, 'copy_of_' + psd_file))
+            shutil.move(os.path.join(source_path, psd_file), os.path.join(output_photoshop, 'copy_of_' + psd_file))
 
     for zip_file in zips:
         file = os.path.join(output_zip, os.path.basename(zip_file))
         if not os.path.exists(file):
-            shutil.move(os.path.join(initial, zip_file), output_zip)
+            shutil.move(os.path.join(source_path, zip_file), output_zip)
         elif os.path.exists(file):
-            shutil.move(os.path.join(initial, zip_file), os.path.join(output_zip, 'copy_of_' + zip_file))
+            shutil.move(os.path.join(source_path, zip_file), os.path.join(output_zip, 'copy_of_' + zip_file))
 
     for code_file in codes:
         file = os.path.join(output_code, os.path.basename(code_file))
         if not os.path.exists(file):
-            shutil.move(os.path.join(initial, code_file), output_code)
+            shutil.move(os.path.join(source_path, code_file), output_code)
         elif os.path.exists(file):
-            shutil.move(os.path.join(initial, code_file), os.path.join(output_code, 'copy_of_' + code_file))
+            shutil.move(os.path.join(source_path, code_file), os.path.join(output_code, 'copy_of_' + code_file))
 
 
 def main():
